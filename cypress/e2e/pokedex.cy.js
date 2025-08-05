759a16b2e2cb6f0d62b8ce007f90cc53fb99a06e
MX_DM <maximodellamaggiore@gmail.com>
2025-08-04 23:29:52 -0300

    Add e2e tests. Add e2e testing to the deployment pipeline. Exercises 2-9 completed

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+describe('Pokedex', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function() {
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('pokemon page can be opened by clicking from the front page', function () {
+    cy.contains('pikachu').click()
+    cy.contains('lightning rod').should('be.visible')
+  })
+})
