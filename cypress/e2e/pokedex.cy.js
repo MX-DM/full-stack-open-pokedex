e8329184edeffcb3750ea8bf576bfad8a5851e4e
MX_DM <maximodellamaggiore@gmail.com>
2025-08-11 19:11:41 -0300

    Force build failure

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,7 +4,7 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
   })
   it('front page can be opened', function() {
-    cy.contains('ivysaur')
+    cy.contains('force failure')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be opened by clicking from the front page', function () {
